00c61e1edd5098d3fe269f60b70f6e525bfb93f1
matam2015 <matam.spring2015@gmail.com>
2015-03-24 20:11:05 +0200

    First draft ex0 - only receives array - unchecked

---
+++ ex0/mtm_tot.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+#define SIZE_ERROR "Invalid size\n"
+#define NUM_ERROR "Invalid number\n"
+
+int main(){
+	int input_array_size, *input_array; //Contains the numbers received 
+	input_array_size = get_array(&input_array);
+	return 0
+}
+
+int get_array(int** input_array){
+	int size; 
+
+	// Get array size
+	printf("Enter size of input:\n");
+	if (scanf(" %d",&size)==0){
+		printf(SIZE_ERROR);
+		return 0;
+	}
+
+	// Check validity
+	if (size<=0){
+		printf(SIZE_ERROR);
+		return 0;
+	}
+	
+	// Allocate array and set input_array to point to it.
+	*input_array = (int *)malloc(sizeof(int)*size);
+	if (*input_array == NULL){
+		return 0;
+	}
+	
+	// Fill array with user inputs
+	printf("Enter numbers:\n");
+	for (int i=0;i<size;i++){
+		if (!scanf(" %d",input_array+i)){ 
+			printf(NUM_ERROR);
+			free(*input_array);
+			return 0;
+		}
+	}
+	//Success!
+	return size;
+}
